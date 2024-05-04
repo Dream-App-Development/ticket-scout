44524bc4b486331264fb5e412702bdbfca656629
Ravshan8860 <127499325+Ravshan88@users.noreply.github.com>
2024-05-04 23:03:00 +0500

    redis set up

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import json
 import re
+import time
 
+import redis
 from playwright.sync_api import sync_playwright
 
 # Declare a global variable to hold the data
 locale_stations_data = None
+main_page_content = None
 
-JAVASCRIPT_FILE_NAME = 'main.259db808914687da4dfa.js'
+JAVASCRIPT_FILE_NAME = 'main.'
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 
 def fix_json(data):
@@ -16,24 +20,42 @@ def fix_json(data):
 
 
 def handle_js_route(route, request):
-    global locale_stations_data
-
     if JAVASCRIPT_FILE_NAME in request.url:
         route.continue_()
         response = request.response()
         if response:
-            body = response.body()
-            js_content = body.decode('utf-8')
-            match = re.search(r'this\.localeStations\s*=\s*(.*?);', js_content)
-            if match:
-                cleaned_data = match.group(1).split(']')[0] + ']'
-                fixed_data = fix_json(cleaned_data)
-                decoded_data = json.loads(fixed_data)
-                print(f"Cleaned data: {decoded_data}")
-            else:
-                print("this.localeStations not found in the JavaScript file.")
+            global main_page_content
+            main_page_content = response.body()
     else:
         route.continue_()
+
+
+def extract_stations_info(body):
+    js_content = body.decode('utf-8')
+    match = re.search(r'this\.localeStations\s*=\s*(.*?);', js_content)
+    if match:
+        return clean_data(match)
+    # print(f"Cleaned data: {user_data}")
+    else:
+        print("this.localeStations not found in the JavaScript file.")
+
+
+def clean_data(match):
+    cleaned_data = match.group(1).split(']')[0] + ']'
+    fixed_data = fix_json(cleaned_data)
+    decoded_data = json.loads(fixed_data)
+    return decoded_data
+
+
+def save_to_redis(decoded_data):
+    for station in decoded_data:
+        key = f"city:{station['code']}"
+        r.set(key, json.dumps(station))
+
+    keys = r.keys('city:*')
+    cities = [json.loads(r.get(key)) for key in
+              keys]  # Using r.get and json.loads to correctly retrieve and deserialize the data
+    print(f"Data from redis:{cities}")
 
 
 def initialize_playwright(playwright):
@@ -48,7 +70,8 @@ def main():
     with sync_playwright() as playwright:
         page = initialize_playwright(playwright)
         page.goto("https://eticket.railway.uz/en/home", timeout=90000)
-        #
+        stations = extract_stations_info(main_page_content)
+        save_to_redis(stations)
 
 
 if __name__ == "__main__":
