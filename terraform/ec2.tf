c1ddd64fbfb3f4d849cecc086e337ffbb2220bdf
Ravshan8860 <127499325+Ravshan88@users.noreply.github.com>
2024-10-25 12:02:23 +0500

    aws_instance off

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,13 +1,13 @@
-resource "aws_instance" "example" {
-  ami = "i-0d375bbd5ddb22172"  # Update with the appropriate AMI ID for your region
-  instance_type = "t2.micro"
-  subnet_id     = data.aws_subnets.subnets.ids[0]
-
-  tags = {
-    Name = "${var.app_ident}-${var.environment}-instance"
-  }
-}
-
-output "instance_id" {
-  value = aws_instance.example.id
-}
+#resource "aws_instance" "example" {
+#  ami = "i-0d375bbd5ddb22172"  # Update with the appropriate AMI ID for your region
+#  instance_type = "t2.micro"
+#  subnet_id     = data.aws_subnets.subnets.ids[0]
+#
+#  tags = {
+#    Name = "${var.app_ident}-${var.environment}-instance"
+#  }
+#}
+#
+#output "instance_id" {
+#  value = aws_instance.example.id
+#}
